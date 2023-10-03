a45a05d0d2514c346fb0f5878cfef1f803db83a6
adityasurana3 <suranaaditya3@gmail.com>
2023-10-03 19:40:28 +0545

    Home layout created in Frontend

---
+++ lms_frontend/src/components/Main.js
@@ -0,0 +1,15 @@
+import Home from './Home'
+import Header from './Header';
+import Footer from './Footer';
+
+function Main(){
+    return(
+        <div className="App">
+            <Header />
+            <Home />
+            <Footer />
+        </div>
+    )
+}
+
+export default Main;
